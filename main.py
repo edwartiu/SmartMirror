600a078187a29eee7d45e4ac1d73dd5521f93e66
edwartiu <etjosue07@gmail.com>
2020-11-13 12:18:50 -0800

    Created googlecalendar.py
    The file grants the app access to the users google calendar and returns a list of events from that user. Still have to format and update so it can be displayed on the main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from datetime import datetime
 
 window = Tk()
+# For Ubuntu or Mac change '-fullscreen' to '-zoomed'
 window.attributes('-fullscreen', True)
 
 # Code for widgets will go under here
@@ -9,7 +10,7 @@ window.attributes('-fullscreen', True)
 # Display Time
 def update_time():
     now = datetime.now()
-    # Change %#I to %-I when using Unix system 
+    # Change %#I to %-I when using Ubuntu or Mac system 
     formatted_time = now.strftime("%#I:%M")
     time.config(text = formatted_time)
     window.after(500, update_time)
