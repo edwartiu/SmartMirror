707398d49c7e08f03cd4d3adecd12ca640dabbe9
edwartiu <etjosue07@gmail.com>
2020-11-16 14:26:33 -0800

    Date displayed
    Current date is now displayed in GUI, working on fomratting the events from google calendar

--- googlecalendar.py
+++ googlecalendar.py
@@ -15,7 +15,7 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 creds = None
 
-# Checks if user credentials are already saved in pkl file. If so loads them to creds var
+# Checks if user credentials are already saved in pkl file. If so assigns thme to creds
 if os.path.exists('token.pkl'):
     with open('token.pkl', 'rb') as token:
         creds = pickle.load(token)
@@ -38,54 +38,25 @@ service = build('calendar', 'v3', credentials=creds)
 # Call the Calendar API
 now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 print('Getting the upcoming 10 events')
-events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=5, singleEvents=True, orderBy='startTime').execute()
 events = events_result.get('items', [])
 
 if not events:
     print('No upcoming events found.')
 for event in events:
-    start = event['start'].get('dateTime', event['start'].get('date'))
-    print(start, event['summary'])
+    information = {}
+    information['title'] = event['summary']
+    startTime = event['start'].get('dateTime')
+    startTime = startTime[:19].replace('T', ' ')
+    date_time_obj = datetime.datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
+    start = date_time_obj.strftime("%#I:%M")
+
+    print(start)
+    print(" ")
 
 
-'''
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pkl'):
-        with open('token.pkl', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pkl', 'wb') as token:
-            pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-
-if __name__ == '__main__':
-    main()'''
+"""
+start = event['start'].get('dateTime', event['start'].get('date'))
+print(start, event['summary'])
+"""
--- main.py
+++ main.py
@@ -2,24 +2,54 @@ from tkinter import *
 from datetime import datetime
 
 window = Tk()
-# For Ubuntu or Mac change '-fullscreen' to '-zoomed'
-window.attributes('-fullscreen', True)
+try:
+    window.attributes('-fullscreen', True)
+except:
+    window.attributes('-zoomed', True)
+window.configure(background='black')
 
 # Code for widgets will go under here
 
 # Display Time
 def update_time():
     now = datetime.now()
-    # Change %#I to %-I when using Ubuntu or Mac system 
-    formatted_time = now.strftime("%#I:%M")
-    time.config(text = formatted_time)
+    try: 
+        formatted_time = now.strftime("%#I:%M")
+    except:
+        formatted_time = now.strftime("%-I:%M")
+    time.config(text=formatted_time)
     window.after(500, update_time)
 
+def update_date():
+    now = datetime.now()
+    try:
+        formatted_date =  now.strftime('%A, %B %#d')
+    except:
+        formatted_date =  now.strftime('%A, %B %-d')
+    date.config(text=formatted_date)
+    window.after(500, update_date)
 
 
-time = Label(window, text= '')
-time.pack()
 
 
+
+time = Label(window, text= '', bg='black', fg='white', font=("Courier", 44))
+time.grid(column=0, row=1)
+
+date = Label(window, text= '', bg='black', fg='white', font=("Courier", 30))
+date.grid(column=0, row=2)
+
+
+
+
+
+
+
+# Exit tkinter
+def exit(event):
+    window.destroy()
+
+window.bind("<Escape>", exit)
 update_time()
+update_date()
 window.mainloop()
