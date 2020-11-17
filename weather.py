2e77b5f2ff917d3d34cc1983cad591590d6b5604
edwartiu <etjosue07@gmail.com>
2020-11-17 11:21:56 -0800

    Added weather.py
    created new file to use openweathermap api to display current weather

---
+++ weather.py
@@ -0,0 +1,7 @@
+
+
+zip_code = '90002'
+api_key = 'e13a6ae1aed8a3f3f032204098724833'
+api_call = "api.openweathermap.org/data/2.5/weather?zip={}&appid={}".format(zip_code, api_key)
+
+print(api_call)
