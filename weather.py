81e757ebe7c5faf77cf3ffb925cd244f2ce96009
edwartiu <etjosue07@gmail.com>
2020-11-17 11:38:45 -0800

    Pickle file for api key

--- weather.py
+++ weather.py
@@ -1,7 +1,14 @@
-
+import pickle
+import os.path
 
 zip_code = '90002'
-api_key = 'e13a6ae1aed8a3f3f032204098724833'
+api_key = None
+with open('weather_token.pkl', 'rb') as token:
+    api_key = pickle.load(token)
 api_call = "api.openweathermap.org/data/2.5/weather?zip={}&appid={}".format(zip_code, api_key)
 
 print(api_call)
+
+
+    
+
